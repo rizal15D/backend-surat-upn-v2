d13d90441b3cba508011504ca3df09b7eace738e
MRizqy2 <mrfrizqy22@gmail.com>
2023-12-24 22:09:10 +0700

    update sequelize

---
+++ src/models/departemen.js
@@ -0,0 +1,23 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Departemen extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Departemen.init({
+    name: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Departemen',
+  });
+  return Departemen;
+};
