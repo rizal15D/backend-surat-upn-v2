20c09b0bc0c2bb9a993a7c9b7ac6d012c7a557bd
MRizqy2 <mrfrizqy22@gmail.com>
2024-01-01 22:54:42 +0700

    update index and config

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
     "development": {
-        "url": process.env.POSTGRES_URL,
+        // "url": process.env.POSTGRES_URL,
         'host': process.env.POSTGRES_HOST,
         'user': process.env.POSTGRES_USER,
         'password': process.env.POSTGRES_PASSWORD,
@@ -17,7 +17,7 @@ module.exports = {
         }
     },
   "test": {
-    "url": process.env.POSTGRES_URL,
+    // "url": process.env.POSTGRES_URL,
     'host': process.env.POSTGRES_HOST,
     'user': process.env.POSTGRES_USER,
     'password': process.env.POSTGRES_PASSWORD,
@@ -32,7 +32,7 @@ module.exports = {
     }
   },
   "production": {
-    "url": process.env.POSTGRES_URL,
+    // "url": process.env.POSTGRES_URL,
     'host': process.env.POSTGRES_HOST,
     'user': process.env.POSTGRES_USER,
     'password': process.env.POSTGRES_PASSWORD,
