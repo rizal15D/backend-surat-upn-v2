77bbcd00c58a49d43fa05d523a6bd5e09bd2746e
MRizqy2 <mrfrizqy22@gmail.com>
2024-01-01 15:11:07 +0700

    update .env

--- src/Http/middleware/authMiddleware.js
+++ src/Http/middleware/authMiddleware.js
@@ -1,17 +1,17 @@
 const jwt = require("jsonwebtoken");
-const config = require("../../../process.env");
+// const config = require("../../../process.env");
 
 module.exports = function (req, res, next) {
   const tokenWithBearer = req.header('Authorization');
   if (!tokenWithBearer) return res.status(401).send('Access Denied: No Token Provided!');
-  if (tokenWithBearer !== `Bearer ${process.env.secret_key}`) {
+  if (tokenWithBearer !== `Bearer ${process.env.SECRET_KEY}`) {
     return res.status(401).end('Unauthorized');
   }
   
   const token = tokenWithBearer.split(' ')[1];
 
   try {
-    const decoded = jwt.verify(token, config.development.secret_key);
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
     req.user = decoded;
     next();
   } catch (error) {
