c46831a887c042b26adf9eda945f2dd6b9319167
MRizqy2 <mrfrizqy22@gmail.com>
2024-01-01 17:32:19 +0700

    update index.js

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
 const port = 3000;
 const router = require("./src/routes/index.js");
 
+const { Sequelize } = require('sequelize');
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(router);
+
+const sequelize = new Sequelize(process.env.POSTGRES_URL, {
+  dialect: 'postgres',
+  dialectModule: require('pg'),
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+
+sequelize.authenticate()
+  .then(() => {
+    console.log("Connected to postgres database!");
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
