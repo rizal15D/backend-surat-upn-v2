adf4ae176e46c7fdeb00085c2bd58a206b54b1e2
rizal15D <muhammadrizal15122001@gmail.com>
2024-01-08 16:58:03 +0700

    nomor surat

--- src/Http/controllers/daftar_surat_controller/status_controller.js
+++ src/Http/controllers/daftar_surat_controller/status_controller.js
@@ -1,7 +1,12 @@
 // const cloudinary = require("../../../../config/cloudinaryConfig");
 // const express = require("express");
 // const app = express();
-// const { Daftar_surat, Template_surat, Role_user } = require("../../../models");
+const {
+  Daftar_surat,
+  Template_surat,
+  Role_user,
+  Users,
+} = require("../../../models");
 
 // const role_user = await Role_user.findOne({ where: { id: req.query.id } });
 // const role_user = await Role_user.findAll();
@@ -24,30 +29,31 @@
 // const isRead = daftar_surat.dibaca === 1; // Assuming the Daftar_surat model has a dibaca field
 
 // const status = getStatus(role_user, isRead);
-// bisa dibuat seperti perulangan aggar lebih efisien/fleksibel
-function getStatus(role_user, isRead, persetujuan) {
-  if (role_user == 3) {
-    return "didaftar tunggu tu";
-  }
-  if (role_user == 2 && isRead) {
-    return "dibaca tu";
-  } else {
-    if (persetujuan) {
-      return "disetujui tu";
-    } else {
-      return "tidak disetujui";
-    }
-  }
-  if (role_user == 4 && !isRead) {
-    return "didaftar tunggu dekan";
-  } else if (role_user == 4 && isRead) {
-    return "dibaca dekan";
-  } else if (role_user == 4 && isRead && persetujuan) {
-    return "disetujui dekan";
-  } else if (role_user == 4 && isRead && !persetujuan) {
-    return "ditolak dekan";
-  }
-}
+
+// function getStatus(role_user, isRead, persetujuan) {
+
+//   if (role_user == 3 && suratStatus) {
+//     return status[0];
+//   }
+//   if (role_user == 2 && isRead) {
+//     return "dibaca tu";
+//   } else if (role_user == 2) {
+//     if (persetujuan) {
+//       return "disetujui tu";
+//     } else {
+//       return "tidak disetujui";
+//     }
+//   }
+//   if (role_user == 4 && !isRead) {
+//     return "didaftar tunggu dekan";
+//   } else if (role_user == 4 && isRead) {
+//     return "dibaca dekan";
+//   } else if (role_user == 4 && isRead && persetujuan) {
+//     return "disetujui dekan";
+//   } else if (role_user == 4 && isRead && !persetujuan) {
+//     return "ditolak dekan";
+//   }
+// }
 
 // function getStatus(role_user) {
 //   console.log(role_user);
@@ -61,5 +67,41 @@ function getStatus(role_user, isRead, persetujuan) {
 //     return "didaftar tunggu dekan";
 //   }
 // }
+
+const status = [
+  "didaftar tunggu tu",
+  "dibaca tu",
+  "disetujui tu",
+  "tidak disetujui",
+  "didaftar tunggu dekan",
+  "dibaca dekan",
+  "disetujui dekan",
+  "ditolak dekan",
+];
+
+function getStatus(user_id, surat_id) {
+  let index = 0;
+
+  const surat = Daftar_surat.findOne({
+    where: { id: surat_id },
+  });
+  surat.status = String(surat.status);
+  const user = Users.findOne({ where: { id: user_id } });
+
+  for (let i = 0; i < status.length(); i++) {
+    if (status[i] == surat.status) {
+      break;
+    }
+  }
+  //prodi
+  if (user.role_id == 3) {
+    return status[0];
+  }
+  //TU
+  else if (user.role_id == 2 && i < 1) {
+    return status[1];
+  } else if (user.role_id == 2 && i < 2) {
+  }
+}
 
 module.exports = getStatus;
